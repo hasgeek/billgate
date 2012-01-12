2b3936deb5f5d285254d1444f13476cf97de9a36
Nigel Babu <nigelbabu@gmail.com>
2012-01-12 18:04:54 +0530

    Added address editing and deleting

--- billgate/views/payment.py
+++ billgate/views/payment.py
@@ -2,15 +2,17 @@ from billgate import app
 from billgate.models import Address
 from billgate.views.login import lastuser
 from billgate.forms import AddressForm
-from flask import render_template, g, flash, redirect, url_for, session
+from flask import render_template, g, flash, redirect, url_for, session, request
 
 @app.route('/address')
 @lastuser.requires_login
-def select_address():
+def select_address(form=None):
+    if form is None:
+        form = AddressForm()
     context = {
         'user': g.user,
         'addresses': Address.objects(user=g.user),
-        'form': AddressForm(),
+        'form': form,
     }
     return render_template('address.html', **context)
 
@@ -27,7 +29,7 @@ def process_select_address():
         return redirect(url_for('select_payment'))
     else:
         flash("Please check your details and try again.", 'error')
-        return select_address(eventname, regform=form)
+        return select_address(form=form)
 
 @app.route('/address/select/<aid>')
 @lastuser.requires_login
@@ -35,6 +37,44 @@ def select_existing_address(aid):
     address = Address.objects(hashkey=aid).first()
     session['address'] = getattr(address, hashkey, None)
     return redirect(url_for('select_payment'))
+
+@app.route('/address/delete/<aid>')
+@lastuser.requires_login
+def delete_address(aid):
+    address = Address.objects(hashkey=aid).first()
+    address.delete()
+    if request.referrer:
+        next = request.referrer
+    else:
+        next = url_for(index)
+    return redirect()
+
+
+@app.route('/address/edit/<aid>')
+@lastuser.requires_login
+def edit_address(aid, form=None):
+    address = Address.objects(hashkey=aid).first()
+    if form is None:
+        form = AddressForm(obj=address)
+    context = {
+        'form': form,
+    }
+    return render_template('edit_address.html', **context)
+
+@app.route('/address/edit/<aid>', methods=['POST'])
+@lastuser.requires_login
+def process_edit_address(aid):
+    address = Address.objects(hashkey=aid).first()
+    form = AddressForm(obj=address)
+    if form.validate_on_submit():
+        address = Address()
+        form.populate_obj(address)
+        address.user = g.user
+        address.save()
+        return redirect(url_for('select_payment'))
+    else:
+        flash("Please check your details and try again.", 'error')
+        return edit_address(address.hashkey, form=form)
 
 @app.route('/payment')
 @lastuser.requires_login
